ac0b3368d7a2b03e70282930a545358f3983ecd3
kpasagada <k.pasagada@gmail.com>
2019-12-06 15:54:43 -0600

    Jest setup with enzyme and ts-jest.

--- frontend/src/components/cluster-dashboard/cluster-list-view/ClusterListView.test.tsx
+++ frontend/src/components/cluster-dashboard/cluster-list-view/ClusterListView.test.tsx
@@ -5,6 +5,7 @@ import { Cluster } from '../Cluster.model';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ClusterListView clusterData={new Cluster()} refreshClusterData={(): void => {}}/>, div);
+  ReactDOM.render(<ClusterListView clusterData={new Cluster()}
+    refreshClusterData={(): void => { }} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
