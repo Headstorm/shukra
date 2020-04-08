1a2d7fb41d27b5bf92fb6b91ccf9a9507d6b33d0
kpasagada <k.pasagada@gmail.com>
2020-04-08 18:49:20 -0500

    GetClusterStats endpoint

--- src/main/scala/com/headstorm/shukra/akkamgt/helper/ExtendedAkkaRoutesProtocol.scala
+++ src/main/scala/com/headstorm/shukra/akkamgt/helper/ExtendedAkkaRoutesProtocol.scala
@@ -4,7 +4,10 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 final case class ShardNames(names: Set[String])
+final case class ErrorMessage(message: String)
 
 trait ExtendedAkkaRoutesProtocol extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val shardNamesFormat: RootJsonFormat[ShardNames] = jsonFormat1(ShardNames)
+  implicit val clusterMemberMessageFormat: RootJsonFormat[ErrorMessage] =
+    jsonFormat1(ErrorMessage)
 }
--- src/main/scala/com/headstorm/shukra/akkamgt/routes/ExtendedAkkaRoutes.scala
+++ src/main/scala/com/headstorm/shukra/akkamgt/routes/ExtendedAkkaRoutes.scala
@@ -1,11 +1,14 @@
 package com.headstorm.shukra.akkamgt.routes
 
-import akka.cluster.sharding.ShardRegion.GetClusterShardingStats
-import akka.cluster.sharding.{ClusterSharding, ShardCoordinator}
+import akka.cluster.sharding.ClusterSharding
+import akka.http.scaladsl.model.{HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.management.scaladsl.{ManagementRouteProvider, ManagementRouteProviderSettings}
-import com.headstorm.shukra.akkamgt.helper.{ExtendedAkkaRoutesProtocol, ShardNames}
+import akka.pattern.AskTimeoutException
+import akka.util.Timeout
+import com.headstorm.shukra.akkamgt.helper.{ErrorMessage, ExtendedAkkaRoutesProtocol, ShardNames}
+import scala.concurrent.duration._
 
 class ExtendedAkkaRoutes extends ManagementRouteProvider with ExtendedAkkaRoutesProtocol {
 
@@ -20,12 +23,49 @@ class ExtendedAkkaRoutes extends ManagementRouteProvider with ExtendedAkkaRoutes
     }
   }
 
+  private def routeGetClusterShards(settings: ManagementRouteProviderSettings): Route = {
+    get {
+      extractActorSystem { implicit system =>
+        extractExecutionContext { implicit executor =>
+          complete {
+            implicit val timeout: Timeout = Timeout(5.seconds)
+            try {
+              val sharding = ClusterSharding(system)
+              val shardRegionName = sharding.shardTypeNames.toList.head
+              sharding
+                .shardRegion(shardRegionName)
+              HttpEntity("helloclustershards")
+            }
+            catch {
+              case _: AskTimeoutException =>
+                StatusCodes.NotFound -> ErrorMessage(
+                  s"Shard Region not responding, may have been terminated")
+              case _: IllegalArgumentException =>
+                StatusCodes.NotFound -> ErrorMessage(s"Shard Region is not started")
+            }
+          }
+        }
+      }
+    }
+  }
+
   override def routes(settings: ManagementRouteProviderSettings): Route = {
     concat(
-      pathPrefix("extended" / "shardnames") {
+      pathPrefix("extended" ) {
         concat(
-          pathEndOrSingleSlash {
-            routeGetShardNames(settings)
+          pathPrefix("shardnames") {
+            concat(
+              pathEndOrSingleSlash {
+                routeGetShardNames(settings)
+              }
+            )
+          },
+          pathPrefix("clustershards") {
+            concat(
+              pathEndOrSingleSlash {
+                routeGetClusterShards(settings)
+              }
+            )
           }
         )
       }
