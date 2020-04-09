74edfcd68b16463837bcfc26405829626f8146b0
Zachary Romick <zromick@headstorm.com>
2020-04-09 11:54:29 -0500

    Added redux devtools extension to app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
+import { 
+  createStore,
+  combineReducers,
+  compose,
+  applyMiddleware
+} from "redux";
 import { Provider } from "react-redux";
 
 import * as serviceWorker from "./serviceWorker";
@@ -9,9 +14,15 @@ import rootReducer from "./rootReducer";
 import "./index.scss";
 import App from "./components/app/App";
 
-const store = createStore(
+const reducer = combineReducers({
   rootReducer,
-  applyMiddleware(thunk)
+});
+
+// @ts-ignore
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer, composeEnhancer(applyMiddleware(thunk)),
 );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
