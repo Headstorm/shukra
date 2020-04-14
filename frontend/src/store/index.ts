6c5909d7fa49b97632452acbb4903439829b5212
Augustine von Trapp <augustinevontrapp@augustinesmbp2.myfiosgateway.com>
2020-04-14 10:26:08 -0400

    re-implement compose enhancer

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from "redux-thunk";
 
 import appReducer from '../reducers'
 
+// @ts-ignore
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   appReducer,
-  applyMiddleware(thunk)
+  composeEnhancer(applyMiddleware(thunk))
 )
 
 export default store
